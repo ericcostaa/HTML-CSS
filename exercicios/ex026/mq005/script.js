9567de1f30e5a3a8ddec4ee6efd421f337153dfb
Eric Costa <ericcosta99@outlook.com>
2022-12-12 14:18:05 -0300

    ex hamb

--- exercicios/ex026/mq005/script.js
+++ exercicios/ex026/mq005/script.js
@@ -6,3 +6,15 @@ function cliqueMenu() {
      imenu.style.display = 'block'
     }
  }
+
+ function mudouTamanho(){
+
+    if (window.innerWidth >= 768){
+
+        imenu.style.display = 'block'
+    }
+
+    else{
+        imenu.style.display = 'none'
+    }
+ }
